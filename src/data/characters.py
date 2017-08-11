676a1547428edb1e6d6fe46957de33e728810f81
fis <ybbs.daans@hotmail.com>
2017-08-11 09:51:53 +0800

    Remove unused code

--- src/data/characters.py
+++ src/data/characters.py
@@ -2,17 +2,15 @@
 File:          dataLoader.py
 Author:        fis
 Created:              2016
-Last modified: 08 Feb 2017
+Last modified: 11 Jul 2017
 '''
 import pickle
 from random import shuffle
 import numpy as np
 import os
-from keras.utils import np_utils
+from keras.utils import np_utils, Sequence
 from configuration import characterRecognizerConfig as config
-from keras.utils import Sequence
 from evaluator import h2l_debug
-# from preprocessing import reform
 
 debuger = h2l_debug.h2l_debugger()
 
@@ -71,80 +69,6 @@ class symbol_sequence(Sequence):
         self.batch_count += 1
         batch = (batchImages, batchLabels)
         return batch
-
-    def dataGenerator(self, idx):
-        file_count = 0
-        filename = config.TRAIN_DATA + str(file_count) + '.pkl'
-        with open(filename, 'rb') as f:
-            characters = pickle.load(f)
-        shuffle(characters)
-        images, labels = zip(*characters)
-        length = len(labels)
-        images = np.array(images, dtype=np.float32)
-        labels = np.array(labels, dtype=np.float32)
-        batchesCount = length // config.BATCH_SIZE
-        for j in range(batchesCount):
-            start = round(config.BATCH_SIZE*j)
-            end = round(config.BATCH_SIZE*(j+1))
-            if start >= length:
-                raise IndexError(
-                    'Start is greater than the dataset size')
-            elif end > length:
-                batchImages = images[start:, ...]
-                batchLabels = labels[start:, ...]
-            else:
-                batchImages = images[start: end, ...]
-                batchLabels = labels[start: end, ...]
-            batchLabels = np_utils.to_categorical(batchLabels,
-                                                  config.CLASS_NUM)
-            # reformedImages = []
-            # for image in batchImages:
-            #     reformed = reform.randomReform(
-            #         image,
-            #         binarizing=False)
-            #     reformedImages.append(reformed)
-            # batchImages = np.array(reformedImages, dtype=np.float32)
-            batch = (batchImages, batchLabels)
-
-        while True:
-            for i in range(config.FILES_COUNT):
-                filename = config.TRAIN_DATA + str(i) + '.pkl'
-                with open(filename, 'rb') as f:
-                    characters = pickle.load(f)
-                shuffle(characters)
-                images, labels = zip(*characters)
-                length = len(labels)
-                images = np.array(images, dtype=np.float32)
-                labels = np.array(labels, dtype=np.float32)
-                batchesCount = length // config.BATCH_SIZE
-                for j in range(batchesCount):
-                    start = round(config.BATCH_SIZE*j)
-                    end = round(config.BATCH_SIZE*(j+1))
-                    if start >= length:
-                        raise IndexError(
-                            'Start is greater than the dataset size')
-                    elif end > length:
-                        batchImages = images[start:, ...]
-                        batchLabels = labels[start:, ...]
-                    else:
-                        batchImages = images[start: end, ...]
-                        batchLabels = labels[start: end, ...]
-                    batchLabels = np_utils.to_categorical(batchLabels,
-                                                          config.CLASS_NUM)
-                    # reformedImages = []
-                    # for image in batchImages:
-                    #     reformed = reform.randomReform(
-                    #         image,
-                    #         binarizing=False)
-                    #     reformedImages.append(reformed)
-                    # batchImages = np.array(reformedImages, dtype=np.float32)
-                    batch = (batchImages, batchLabels)
-                    yield batch
-
-
-# def trainDataLoader():
-#     loader = dataGenerator()
-#     return loader
 
 
 def validationDataLoader():
