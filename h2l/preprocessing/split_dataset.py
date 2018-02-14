389c455bc58fda16de04699a4dd90e26b36881dc
fis <ybbs.daans@hotmail.com>
2018-02-14 09:11:32 +0800

    Add interface for preprocessing commands, and split_dataset command.
    * h2l/preprocessing/split_dataset.py: New file.
    * h2l_commands.py: New file.

---
+++ h2l/preprocessing/split_dataset.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+#
+# Copyright © 2018 Fis Trivial <ybbs.daans@hotmail.com>
+#
+# This file is part of H2L.
+#
+# H2L is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# H2L is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with H2L.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import os
+from ..configuration import characterRecognizerConfig as config
+from shutil import copyfile
+import sys
+
+
+def split(path):
+    os.listdir(os.path.abspath(path))
+    pardir = os.path.join(path, os.path.pardir)
+    pardir = os.path.normpath(pardir)
+
+    train_dir = os.path.join(pardir, 'train_data')
+    valid_dir = os.path.join(pardir, 'valid_data')
+    if os.path.exists(train_dir):
+        os.system('rm -r ' + train_dir)
+    os.mkdir(train_dir)
+
+    if os.path.exists(valid_dir):
+        os.system('rm -r ' + valid_dir)
+    os.mkdir(valid_dir)
+    symbols = os.listdir(path)
+
+    for sym in symbols:
+        sym_path = os.path.join(path, sym)
+        images = os.listdir(sym_path)
+        dst_train_sym_path = os.path.join(train_dir, sym)
+        dst_valid_sym_path = os.path.join(valid_dir, sym)
+
+        if not os.path.exists(dst_train_sym_path):
+            os.mkdir(dst_train_sym_path)
+        if not os.path.exists(dst_valid_sym_path):
+            os.mkdir(dst_valid_sym_path)
+
+        train_size = int(len(images) * config.TV_RATIO)
+        train_data = images[:train_size]
+        valid_data = images[train_size:]
+        for img in train_data:
+            src = os.path.join(sym_path, img)
+            dst = os.path.join(dst_train_sym_path, img)
+            copyfile(src, dst)
+        for img in valid_data:
+            src = os.path.join(sym_path, img)
+            dst = os.path.join(dst_valid_sym_path, img)
+            copyfile(src, dst)
+
+
+if __name__ == '__main__':
+    path = sys.argv[1]
+    split(path)
---
+++ h2l_commands.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+#
+# Copyright © 2018 Fis Trivial <ybbs.daans@hotmail.com>
+#
+# This file is part of H2L.
+#
+# H2L is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# H2L is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with H2L.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import argparse
+import sys
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Various commands for H2L.',
+        formatter_class=argparse.RawTextHelpFormatter
+    )
+    parser.add_argument(
+        '--split_data', type=str,
+        help='''Split a dataset dir into train and valid, new dirs will be
+located at the same level as the input dir.
+Example:
+        h2l_commands --split_data ../data/pngs/'''
+    )
+    args = parser.parse_args()
+    if args.split_data:
+        from h2l.preprocessing.split_dataset import split
+        split(sys.argv[2])
