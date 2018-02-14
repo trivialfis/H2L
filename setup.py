2fa3ab1497fd9e4c9ffac997f474a712da9f6bb0
fis <ybbs.daans@hotmail.com>
2018-02-14 09:27:21 +0800

    Add setup template.
    * setup.py: New file.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name='H2L',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=['numpy>1.12.1'],
+    project_urls={
+        'Source Code': 'https://github.com/trivialfis/H2L'
+    }
+)
