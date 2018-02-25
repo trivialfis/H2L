840b27b265230ea806138a1faa10228d7221bc90
fis <ybbs.daans@hotmail.com>
2018-02-26 02:17:34 +0800

    Clear Keras sessions in tests.
    * tests/test_model.py: Likewise.

--- tests/test_model.py
+++ tests/test_model.py
@@ -21,21 +21,25 @@
 from H2L.trainer import cnns
 from H2L.trainer import resnet
 from keras.models import Model
+from keras import backend as K
 
 
 def test_build_cnn_sequence():
     model, paras = cnns.sequentialModel(35)
     assert (isinstance(model, Model) and type(paras) is dict
             and len(paras.items()) != 0)
+    K.clear_session()
 
 
 def test_build_cnn_branch():
     model, paras = cnns.sequentialModel(35)
     assert (isinstance(model, Model) and type(paras) is dict
             and len(paras.items()) != 0)
+    K.clear_session()
 
 
 def test_build_res():
     model, paras = resnet.res32(35)
     assert (isinstance(model, Model) and type(paras) is dict
             and len(paras.items()) != 0)
+    K.clear_session()
